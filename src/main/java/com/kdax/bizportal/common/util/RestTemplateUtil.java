495a507d7778439a507d2b761ed8577eaee37a6f
오의섭 <ohus1989@korfinholdings.com>
2020-09-08 16:10:45 +0900

    rest template with header method add

--- src/main/java/com/kdax/bizportal/common/util/RestTemplateUtil.java
+++ src/main/java/com/kdax/bizportal/common/util/RestTemplateUtil.java
@@ -17,12 +17,13 @@ public class RestTemplateUtil {
 
     /**
      * restTemplate exchange
+     *
      * @param httpMethod
      * @param url
      * @param jsonMessage
      * @return HashMap<String, Object> result
      */
-    public HashMap<String, Object> restTemplateExchange(HttpMethod httpMethod, String url, String jsonMessage){
+    public HashMap<String, Object> restTemplateExchange(HttpMethod httpMethod, String url, String jsonMessage) {
         HashMap<String, Object> result = new HashMap<String, Object>();
 
         try {
@@ -45,10 +46,49 @@ public class RestTemplateUtil {
 
         } catch (HttpClientErrorException | HttpServerErrorException e) {
             result.put("statusCode", e.getRawStatusCode());
-            result.put("body"  , e.getStatusText());
+            result.put("body", e.getStatusText());
         } catch (Exception e) {
             result.put("statusCode", "999");
-            result.put("body"  , e.getMessage());
+            result.put("body", e.getMessage());
+        }
+
+        return result;
+
+    }
+
+    /**
+     * restTemplate exchange
+     *
+     * @param httpMethod
+     * @param url
+     * @param jsonMessage
+     * @param header
+     * @return HashMap<String, Object> result
+     */
+    public HashMap<String, Object> restTemplateExchange(HttpMethod httpMethod, String url, String jsonMessage, HttpHeaders header) {
+        HashMap<String, Object> result = new HashMap<String, Object>();
+
+        try {
+            HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+            factory.setConnectTimeout(5000);
+            factory.setReadTimeout(5000);
+            RestTemplate restTemplate = new RestTemplate(factory);
+
+            HttpEntity<String> entity = new HttpEntity<String>(jsonMessage.toString(), header);
+            UriComponents uri = UriComponentsBuilder.fromHttpUrl(url).build();
+
+            ResponseEntity<Map> resultMap = restTemplate.exchange(uri.toString(), httpMethod, entity, Map.class);
+
+            result.put("statusCode", resultMap.getStatusCodeValue());
+            result.put("header", resultMap.getHeaders());
+            result.put("body", resultMap.getBody());
+
+        } catch (HttpClientErrorException | HttpServerErrorException e) {
+            result.put("statusCode", e.getRawStatusCode());
+            result.put("body", e.getStatusText());
+        } catch (Exception e) {
+            result.put("statusCode", "999");
+            result.put("body", e.getMessage());
         }
 
         return result;
