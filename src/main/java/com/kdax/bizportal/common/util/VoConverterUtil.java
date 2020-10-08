2017820ab6e463444657ab579a5fecbe65630a93
오의섭 <ohus1989@korfinholdings.com>
2020-10-08 18:45:46 +0900

    map to vo util 추가

--- src/main/java/com/kdax/bizportal/common/util/VoConverterUtil.java
+++ src/main/java/com/kdax/bizportal/common/util/VoConverterUtil.java
@@ -12,6 +12,7 @@ import java.beans.PropertyDescriptor;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 @UtilityClass
@@ -99,5 +100,34 @@ public class VoConverterUtil {
                 return true;
         }
         return false;
+    }
+
+    /**
+     * Map을 Vo로 변환
+     * @param map
+     * @param obj
+     * @return
+     */
+    public static Object convertMapToObject(Map<String,Object> map,Object obj){
+        String keyAttribute = null;
+        String setMethodString = "set";
+        String methodString = null;
+        Iterator itr = map.keySet().iterator();
+
+        while(itr.hasNext()){
+            keyAttribute = (String) itr.next();
+            methodString = setMethodString+keyAttribute.substring(0,1).toUpperCase()+keyAttribute.substring(1);
+            Method[] methods = obj.getClass().getDeclaredMethods();
+            for(int i=0;i<methods.length;i++){
+                if(methodString.equals(methods[i].getName())){
+                    try{
+                        methods[i].invoke(obj, map.get(keyAttribute));
+                    }catch(Exception e){
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+        return obj;
     }
 }
