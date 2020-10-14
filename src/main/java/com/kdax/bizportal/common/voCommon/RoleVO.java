557ad1f20ab311e44b94856c6bf029f5d36ec0f9
taekwang.lee <taekwang.lee@korfinholdings.com>
2020-10-14 17:13:13 +0900

    #358 전자계약조회 권한 체크 전체조회추가

---
+++ src/main/java/com/kdax/bizportal/common/voCommon/RoleVO.java
@@ -0,0 +1,15 @@
+package com.kdax.bizportal.common.voCommon;
+
+import lombok.Data;
+
+@Data
+public class RoleVO {
+    private String account;
+    private String userName;
+    private String roleId;
+    private String roleName;
+    private String roleLevel;
+    private String roleDesc;
+    private String userCodeId;
+    private String grantDa;
+}
