25a59b71e3a26926689c79e84bb5a5c4b8e7da67
taekwang.lee <taekwang.lee@korfinholdings.com>
2020-10-23 21:27:14 +0900

    #316 전자계약 완료 데이터 매출테이블과 연계

--- src/main/java/com/kdax/bizportal/common/voCommon/UserVO.java
+++ src/main/java/com/kdax/bizportal/common/voCommon/UserVO.java
@@ -27,4 +27,8 @@ public class UserVO {
     private String eSignOnId;
     private String eSignOnPwd;
     private String fcmToken;
+    private String deptCode;
+    private String officersNumber;
+    private String rcmndNumber;
+
 }
