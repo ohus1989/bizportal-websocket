76f8859c29deaaaa13c3e466acfe6126c390173c
오의섭 <ohus1989@korfinholdings.com>
2020-10-31 19:11:46 +0900

    공통 vo 추가

---
+++ src/main/java/com/kdax/bizportal/common/voCommon/ComVO.java
@@ -0,0 +1,11 @@
+package com.kdax.bizportal.common.voCommon;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ComVO {
+    private String codeId;
+    private String remoteIp;
+}
