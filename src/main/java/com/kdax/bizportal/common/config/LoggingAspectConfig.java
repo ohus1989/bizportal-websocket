a7c72b4c53937bcea778820d2c31d281f5509b76
taekwang.lee <taekwang.lee@korfinholdings.com>
2020-11-03 21:01:09 +0900

    공통 로그 추가

--- src/main/java/com/kdax/bizportal/common/config/LoggingAspectConfig.java
+++ src/main/java/com/kdax/bizportal/common/config/LoggingAspectConfig.java
@@ -1,88 +1,72 @@
-//package com.kdax.bizportal.common.config;
+package com.kdax.bizportal.common.config;
+
+import com.google.common.base.Joiner;
+import com.google.gson.Gson;
+import com.kdax.bizportal.common.exception.GlobalExceptionHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Component // 1
+@Aspect // 2
+@Slf4j
+public class LoggingAspectConfig {
+
+    @Autowired
+    Environment env;
+
+    @Autowired
+    GlobalExceptionHandler globalExceptionHandler;
+
+    Gson gson = new Gson();
+
+    @Autowired
+    private HttpServletResponse response;
+
+    private String paramMapToString(Map<String, String[]> paramMap) {
+        return paramMap.entrySet().stream()
+                .map(entry -> String.format("%s -> (%s)",
+                        entry.getKey(), Joiner.on(",").join(entry.getValue())))
+                .collect(Collectors.joining(", "));
+    }
+
+//    @Pointcut("within(com.kdax.bizportal.auth.modules...*)") // 3
+//    public void onRequest() {
 //
-//import com.google.common.base.Joiner;
-//import com.google.gson.Gson;
-//import com.kdax.bizportal.common.exception.BizExceptionMessage;
-//import com.kdax.bizportal.common.exception.GlobalExceptionHandler;
-//import com.kdax.bizportal.common.voCommon.LogComVO;
-//import com.kdax.bizportal.common.voCommon.LogReqVO;
-//import com.kdax.bizportal.common.voCommon.LogResVO;
-//import lombok.extern.slf4j.Slf4j;
-//import org.apache.commons.io.IOUtils;
-//import org.aspectj.lang.ProceedingJoinPoint;
-//import org.aspectj.lang.Signature;
-//import org.aspectj.lang.annotation.Around;
-//import org.aspectj.lang.annotation.Aspect;
-//import org.slf4j.MDC;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.core.env.Environment;
-//import org.springframework.http.HttpHeaders;
-//import org.springframework.stereotype.Component;
-//import org.springframework.web.context.request.RequestContextHolder;
-//import org.springframework.web.context.request.ServletRequestAttributes;
-//
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//import java.time.LocalDateTime;
-//import java.time.format.DateTimeFormatter;
-//import java.util.*;
-//import java.util.function.Function;
-//import java.util.stream.Collectors;
-//
-//@Component // 1
-//@Aspect // 2
-//@Slf4j
-//public class LoggingAspectConfig {
-//
-//    @Autowired
-//    Environment env;
-//
-//    @Autowired
-//    GlobalExceptionHandler globalExceptionHandler;
-//
-//    Gson gson = new Gson();
-//
-//    @Autowired
-//    private HttpServletResponse response;
-//
-//    private String paramMapToString(Map<String, String[]> paramMap) {
-//        return paramMap.entrySet().stream()
-//                .map(entry -> String.format("%s -> (%s)",
-//                        entry.getKey(), Joiner.on(",").join(entry.getValue())))
-//                .collect(Collectors.joining(", "));
+//        log.debug("LoggingAspectConfig!!!!!!!!! onRequest");
 //    }
+
+//    @Before("execution(* com.kdax.bizportal..**Api.*(..))")
+//    public void startLog(JoinPoint jp) {
 //
-////    @Pointcut("within(com.kdax.bizportal.auth.modules...*)") // 3
-////    public void onRequest() {
-////
-////        log.debug("LoggingAspectConfig!!!!!!!!! onRequest");
-////    }
+//        log.info("-------------------------------------");
+//        log.info("-------------------------------------");
 //
-////    @Before("execution(* com.kdax.bizportal..**Api.*(..))")
-////    public void startLog(JoinPoint jp) {
-////
-////        log.info("-------------------------------------");
-////        log.info("-------------------------------------");
-////
-////        /* 전달되는 모든 파라미터들을 Object의 배열로 가져온다. */
-////        log.info("1:" + new Gson().toJson(jp.getArgs()));
-////
-////        /* 해당 Advice의 타입을 알아낸다. */
-////        log.info("2:" + jp.getKind());
-////
-////        /* 실행하는 대상 객체의 메소드에 대한 정보를 알아낼 때 사용 */
-////        log.info("3:" + jp.getSignature().getName());
-////
-////        /* target 객체를 알아낼 때 사용 */
-////        log.info("4:" + jp.getTarget().toString());
-////
-////        /* Advice를 행하는 객체를 알아낼 때 사용 */
-////        log.info("5:" + jp.getThis().toString());
-////
-////    }
+//        /* 전달되는 모든 파라미터들을 Object의 배열로 가져온다. */
+//        log.info("1:" + new Gson().toJson(jp.getArgs()));
 //
-//    //    @Around("execution(* com.kdax.bizportal.basicbiz.modules.coinevent.CoinEventApi.selectCoinEventList(..))")
-//    // 4
+//        /* 해당 Advice의 타입을 알아낸다. */
+//        log.info("2:" + jp.getKind());
+//
+//        /* 실행하는 대상 객체의 메소드에 대한 정보를 알아낼 때 사용 */
+//        log.info("3:" + jp.getSignature().getName());
+//
+//        /* target 객체를 알아낼 때 사용 */
+//        log.info("4:" + jp.getTarget().toString());
+//
+//        /* Advice를 행하는 객체를 알아낼 때 사용 */
+//        log.info("5:" + jp.getThis().toString());
+//
+//    }
+
+    //    @Around("execution(* com.kdax.bizportal.basicbiz.modules.coinevent.CoinEventApi.selectCoinEventList(..))")
+    // 4
 //    @Around("execution(* com.kdax.bizportal..**Api.*(..))")
 //    public Object doLogging(ProceedingJoinPoint pjp) throws Throwable {
 //        String lockENV = env.getProperty("spring.profiles.active");
@@ -171,5 +155,5 @@
 //        }
 //
 //    }
-//
-//}
+
+}
