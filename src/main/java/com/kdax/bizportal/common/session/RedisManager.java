437ff51be5012c7e2b9db2b20601726a8a56436c
taekwang.lee <taekwang.lee@korfinholdings.com>
2020-11-23 14:07:23 +0900

    RedisTemplate -> StringRedisTemplate 변경

--- src/main/java/com/kdax/bizportal/common/session/RedisManager.java
+++ src/main/java/com/kdax/bizportal/common/session/RedisManager.java
@@ -1,10 +1,12 @@
 package com.kdax.bizportal.common.session;
 
+import com.google.gson.Gson;
 import com.kdax.bizportal.common.constants.GlobalConstants;
 import com.kdax.bizportal.common.voCommon.MenuVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Component;
 
@@ -18,32 +20,35 @@ public class RedisManager {
     @Autowired
     RedisTemplate<String, Object> redisTemplate;
 
-    public void setValue(String key, Object value){
-        ValueOperations<String, Object> vop = redisTemplate.opsForValue();
+    @Autowired
+    StringRedisTemplate stringRedisTemplate;
+
+    public void setValue(String key, String value){
+        ValueOperations<String, String> vop = stringRedisTemplate.opsForValue();
         //set
         vop.set(key, value);
-        redisTemplate.expire(key, GlobalConstants.DEFALUT_REDIS_TIMEOUT_USERINFO, TimeUnit.SECONDS);
+        stringRedisTemplate.expire(key, GlobalConstants.DEFALUT_REDIS_TIMEOUT_USERINFO, TimeUnit.SECONDS);
         return;
     }
 
     public void setUserSessionInfo(String userCodeId, List<MenuVO> simpleList){
-        ValueOperations<String, Object> vop = redisTemplate.opsForValue();
+        ValueOperations<String, String> vop = stringRedisTemplate.opsForValue();
         //자료형 생성
         UserRedisInfoVO setData = new UserRedisInfoVO();
         setData.setId(userCodeId);
         setData.setMenuPriviledge(simpleList);
 
         //set
-        vop.set(userCodeId, setData);
-        redisTemplate.expire(userCodeId, GlobalConstants.DEFALUT_REDIS_TIMEOUT_USERINFO, TimeUnit.SECONDS);
+        vop.set(userCodeId, new Gson().toJson(setData));
+        stringRedisTemplate.expire(userCodeId, GlobalConstants.DEFALUT_REDIS_TIMEOUT_USERINFO, TimeUnit.SECONDS);
         
         log.info("set Redis UserInfo : " + userCodeId);
         return;
     }
 
-    public Object getValue(String key){
-        ValueOperations<String, Object> vop = redisTemplate.opsForValue();
-        redisTemplate.expire(key, GlobalConstants.DEFALUT_REDIS_TIMEOUT_USERINFO, TimeUnit.SECONDS);
+    public String getValue(String key){
+        ValueOperations<String, String> vop = stringRedisTemplate.opsForValue();
+        stringRedisTemplate.expire(key, GlobalConstants.DEFALUT_REDIS_TIMEOUT_USERINFO, TimeUnit.SECONDS);
         return vop.get(key);
     }
 
