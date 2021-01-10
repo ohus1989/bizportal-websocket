0bb8ed81e5a23cfdadced5670bc896035311d13e
taekwang.lee <taekwang.lee@korfinholdings.com>
2021-01-11 08:55:06 +0900

    날짜 유효성 체크 null|blank 체크 안함

--- src/main/java/com/kdax/bizportal/common/validation/BizDateValidator.java
+++ src/main/java/com/kdax/bizportal/common/validation/BizDateValidator.java
@@ -7,12 +7,17 @@ import java.text.SimpleDateFormat;
 public class BizDateValidator implements ConstraintValidator<BizDate, String> {
 
     private String pattern;
+
     @Override public void initialize(BizDate constraintAnnotation) {
         pattern = constraintAnnotation.pattern();
     }
 
     @Override
     public boolean isValid(String value, ConstraintValidatorContext context) {
+
+        if (value == null || "".equals(value)) {
+            return true;
+        }
 
         SimpleDateFormat df1 = new SimpleDateFormat(pattern);
         df1.setLenient(false);
