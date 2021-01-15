ed5c33b77a97eaa4b3e47e097882a14646e0d635
오의섭 <ohus1989@korfinholdings.com>
2021-01-15 16:32:45 +0900

    vo converter util sonar lint

--- src/main/java/com/kdax/bizportal/common/util/VoConverterUtil.java
+++ src/main/java/com/kdax/bizportal/common/util/VoConverterUtil.java
@@ -7,9 +7,11 @@ import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 
 import java.beans.BeanInfo;
+import java.beans.IntrospectionException;
 import java.beans.Introspector;
 import java.beans.PropertyDescriptor;
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -19,8 +21,10 @@ import java.util.Map;
 @Slf4j
 public class VoConverterUtil {
     private static final String[] notExistArr = new String[]{"class"};
+
     /**
      * 모든 항목 맵에 추가
+     *
      * @param paramObj
      * @return
      */
@@ -29,7 +33,7 @@ public class VoConverterUtil {
         MultiValueMap<String, String> paramMap = new LinkedMultiValueMap<>();
 
         for (Field tmpField : vo.getDeclaredFields()) {
-            String val = (String)getValueFromField(tmpField, vo, paramObj);
+            String val = (String) getValueFromField(tmpField, vo, paramObj);
             paramMap.add(tmpField.getName(), val);
             log.debug("Field Name : " + tmpField.getName() + "Field Value : " + val);
         }
@@ -39,6 +43,7 @@ public class VoConverterUtil {
 
     /**
      * 값이 있는 항목만 맵에 추가
+     *
      * @param paramObj
      * @return
      */
@@ -47,8 +52,8 @@ public class VoConverterUtil {
         MultiValueMap<String, String> paramMap = new LinkedMultiValueMap<>();
 
         for (Field tmpField : vo.getDeclaredFields()) {
-            String val = (String)getValueFromField(tmpField, vo, paramObj);
-            if(val!=null && !val.equals(""))
+            String val = (String) getValueFromField(tmpField, vo, paramObj);
+            if (val != null && !val.equals(""))
                 paramMap.add(tmpField.getName(), val);
             log.debug("Field Name : " + tmpField.getName() + "Field Value : " + val);
         }
@@ -57,12 +62,12 @@ public class VoConverterUtil {
     }
 
     private Object getValueFromField(Field field, Class<?> clazz, Object obj) {
-        for(Method method : clazz.getMethods()) {
+        for (Method method : clazz.getMethods()) {
             String methodName = method.getName();
-            if( (methodName.startsWith("get") && methodName.length() == field.getName().length() + 3)
-                    || (methodName.startsWith("is") && methodName.length() == field.getName().length() + 2) ) {
+            if ((methodName.startsWith("get") && methodName.length() == field.getName().length() + 3)
+                    || (methodName.startsWith("is") && methodName.length() == field.getName().length() + 2)) {
 
-                if(methodName.toLowerCase().endsWith(field.getName().toLowerCase())) {
+                if (methodName.toLowerCase().endsWith(field.getName().toLowerCase())) {
                     try {
                         return method.invoke(obj);
                     } catch (Exception e) {
@@ -76,18 +81,18 @@ public class VoConverterUtil {
     }
 
 
-    public static Map domainToMap(Object vo) throws Exception {
+    public static Map domainToMap(Object vo) throws IllegalAccessException, IntrospectionException, InvocationTargetException {
         return domainToMapWithExcept(vo, notExistArr);
-//        return domainToMapWithExcept(vo, null);
     }
 
-    public static Map<String, Object> domainToMapWithExcept(Object vo, String[] arrExceptList) throws Exception {
-        Map<String, Object> result = new HashMap<String, Object>();
+    public static Map<String, Object> domainToMapWithExcept(Object vo, String[] arrExceptList) throws IntrospectionException, InvocationTargetException, IllegalAccessException {
+        Map<String, Object> result = new HashMap<>();
         BeanInfo info = Introspector.getBeanInfo(vo.getClass());
         for (PropertyDescriptor pd : info.getPropertyDescriptors()) {
             Method reader = pd.getReadMethod();
             if (reader != null) {
-                if(arrExceptList != null && arrExceptList.length > 0 && isContain(arrExceptList, pd.getName())) continue;
+                if (arrExceptList != null && arrExceptList.length > 0 && isContain(arrExceptList, pd.getName()))
+                    continue;
                 result.put(pd.getName(), reader.invoke(vo));
             }
         }
@@ -104,25 +109,26 @@ public class VoConverterUtil {
 
     /**
      * Map을 Vo로 변환
+     *
      * @param map
      * @param obj
      * @return
      */
-    public static Object convertMapToObject(Map<String,Object> map,Object obj){
+    public static Object convertMapToObject(Map<String, Object> map, Object obj) {
         String keyAttribute = null;
         String setMethodString = "set";
         String methodString = null;
         Iterator itr = map.keySet().iterator();
 
-        while(itr.hasNext()){
+        while (itr.hasNext()) {
             keyAttribute = (String) itr.next();
-            methodString = setMethodString+keyAttribute.substring(0,1).toUpperCase()+keyAttribute.substring(1);
+            methodString = setMethodString + keyAttribute.substring(0, 1).toUpperCase() + keyAttribute.substring(1);
             Method[] methods = obj.getClass().getDeclaredMethods();
-            for(int i=0;i<methods.length;i++){
-                if(methodString.equals(methods[i].getName())){
-                    try{
+            for (int i = 0; i < methods.length; i++) {
+                if (methodString.equals(methods[i].getName())) {
+                    try {
                         methods[i].invoke(obj, map.get(keyAttribute));
-                    }catch(Exception e){
+                    } catch (Exception e) {
                         e.printStackTrace();
                     }
                 }
