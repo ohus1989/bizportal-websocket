4e40f94d8b9162b81e6acf919bebec41ec53907f
jjwon0409 <jjwon0409@korfinholdings.com>
2022-05-18 15:09:39 +0900

    2479 연장 기능 개발 => 테스트

--- src/main/java/com/kdax/bizportal/common/session/VerifyToken.java
+++ src/main/java/com/kdax/bizportal/common/session/VerifyToken.java
@@ -136,6 +136,7 @@ public class VerifyToken {
 
     public AuthTokenVO getAuthTokenFromString(String token) {
         AuthTokenVO authTokenVO = null;
+
         try {
             if (StringUtils.isEmpty(token)) {
                 log.warn("invaild token {}", token);
@@ -170,6 +171,11 @@ public class VerifyToken {
 
             Calendar today = Calendar.getInstance();
             today.setTime(new Date());
+            log.info("today : "+today.getTime().toString());
+            log.info("getExpireDate : "+authTokenVO.getExpireDate().toString());
+            log.info(authTokenVO
+                    .getExpireDate()
+                    .compareTo(today.getTime()) ==-1? "true" : "false");
 
             if (authTokenVO
                     .getExpireDate()
@@ -205,6 +211,10 @@ public class VerifyToken {
             } else {
                 cal.add(Calendar.MINUTE, webExpireMinute);
             }
+
+            log.info("webExpireMinute : "+String.valueOf(webExpireMinute));
+            log.info("cal.getTime() : "+String.valueOf(cal.getTime())); //Wed May 18 14:07:53 KST 2022
+            log.info("now : "+new Date()); //Wed May 18 14:06:53 KST 2022
 
             authTokenVO.setUserCodeId(userInfoVO.getCodeId());
 
